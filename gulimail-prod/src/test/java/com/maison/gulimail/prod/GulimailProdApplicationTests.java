b0bcd09d8c9bac35ad30fef2eebdb76e2c3e1ba1
ShunheWang <maisonwangsh@gmail.com>
2023-07-27 17:49:27 +0800

    项目初始化

---
+++ gulimail-prod/src/test/java/com/maison/gulimail/prod/GulimailProdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maison.gulimail.prod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailProdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimail-ware/src/test/java/com/maison/gulimail/ware/GulimailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maison.gulimail.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
